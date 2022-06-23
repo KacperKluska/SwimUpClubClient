aba7aa28a768786c5f25c5544d9356ec4e81ccd1
KacperKluska <kacperkluska130@gmail.com>
2022-06-23 21:07:47 +0200

    change protected route to work only with logged users

--- src/components/ProtectedRoute/ProtectedRoute.component.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.component.tsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
+import { UserContext } from '../../context/UserContext';
 import { LoginPage } from '../../pages/LoginPage/LoginPage.component';
 
 export const ProtectedRoute = () => {
-  const auth = true;
+  const { isLogged } = useContext(UserContext);
+  const auth = isLogged?.userLogged;
   return auth ? <Outlet /> : <LoginPage />;
 };
