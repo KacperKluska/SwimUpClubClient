f61bb578d57300346aa2b891c603ac8a5312e613
KacperKluska <kacperkluska130@gmail.com>
2022-08-02 21:43:22 +0200

    added useLocalStorage Hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,34 @@
+import { useContext, useState } from 'react';
+import { SnackBarContext } from '../context/SnackBarContext';
+
+export const useLocalStorage = (key: string, initialValue: string) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') {
+      return initialValue;
+    }
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+  const { setSnackBar } = useContext(SnackBarContext);
+
+  const setValue = (value: object) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== 'undefined') {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      setSnackBar(
+        'Unknown error occurred. Check useLocalStorage Hook.',
+        'error',
+      );
+    }
+  };
+  return [storedValue, setValue];
+};
